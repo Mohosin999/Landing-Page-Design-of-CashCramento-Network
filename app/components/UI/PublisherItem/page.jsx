dfc9e9ed21d8e7896c3256771c551a80541a9bed
Mohosin999 <mohosin.h.akash@gmail.com>
2024-03-04 11:29:09 +0600

    done

--- app/components/UI/PublisherItem/page.jsx
+++ app/components/UI/PublisherItem/page.jsx
@@ -7,7 +7,7 @@ const PublisherItem = ({ title, image, className }) => {
       className={`flex flex-col items-center justify-center px-4 py-8 shadow-lg bg-gray-200 dark:shadow-gray-900 dark:bg-gray-800 ${className}`}
     >
       <div className="">
-        <Image src={image} className="w-32 h-auto" />
+        <Image src={image} alt='image' className="w-32 h-auto" />
       </div>
       <p className="mt-3 lg:mt-5 text-sm">{title}</p>
     </div>
